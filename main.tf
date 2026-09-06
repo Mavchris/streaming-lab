edbf2cccf150194298633fce89883a937a2526ae
Mavchris <bellchrismv@gmail.com>
2026-09-06 19:46:44 -0400

    creation du vpc principal

--- main.tf
+++ main.tf
@@ -1,2 +1,8 @@
-provider "aws" {region = "eu-west-3"}
+provider "aws" { region = "eu-west-3" }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = { Name = "streaming_lab_vpc" }
+
+}
