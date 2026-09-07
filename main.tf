17787ed7a5354d41e26c82e34202e01f100685b5
Mavchris <bellchrismv@gmail.com>
2026-09-07 06:19:51 -0400

    insertion d'un sous reseau; d'une passerelle et d'une table de routage

--- main.tf
+++ main.tf
@@ -6,3 +6,33 @@ resource "aws_vpc" "main" {
   tags = { Name = "streaming_lab_vpc" }
 
 }
+# 1. Sous-réseau public
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags                    = { Name = "streaming_public_subnet" }
+}
+# 2. Passerelle Internet
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "streaming-lab-gateway" }
+
+}
+# 3. Table de routage pointant vers l'IGW
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = { Name = "streaming-lab-public-route" }
+
+}
+
+# 4. Association du sous-réseau à la table de routage
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
