6d8ca9ce20586b6f5260af44e128137dac78765f
Mavchris <bellchrismv@gmail.com>
2026-09-10 03:01:15 -0400

    creation du load balancer et de ses composant et attachement au serverweb

--- main.tf
+++ main.tf
@@ -139,6 +139,15 @@ resource "aws_security_group" "frontend_sg" {
   }
 
   ingress {
+    description     = "trafic entrant depuis le load balancer"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+
+  ingress {
     description     = "ingestion des flux video depuis le serveur de streaming tier2 uniquement"
     from_port       = 1935
     to_port         = 1935
@@ -282,6 +291,50 @@ resource "aws_instance" "database" {
   tags = { Name = "tier3-database" }
 }
 
+#D creation des instances load balancer
+#creation du load balancer
+resource "aws_lb" "main" {
+  name               = "streaming-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.alb1.id, aws_subnet.alb2.id]
+
+  tags = { Name = "streaming-lab-alb" }
+}
+
+resource "aws_lb_target_group" "frontend" {
+  name     = "frontend-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = { Name = "streaming-lab-frontend-tg" }
+}
+
+
+resource "aws_lb_target_group_attachment" "frontend" {
+  target_group_arn = aws_lb_target_group.frontend.arn
+  target_id        = aws_instance.frontend.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
 
 #creation d'un parametre ssm pour; d'un iam-role et d'une policy lié a cet iam
 
